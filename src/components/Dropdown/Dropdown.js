3b41479075caf91f8b1ba15d0ca42d16afc698c4
TatianaKh0301 <92024678+TatianaKh0301@users.noreply.github.com>
2022-11-21 17:58:23 +0200

    finish Counter, Dropdown

---
+++ src/components/Dropdown/Dropdown.js
@@ -0,0 +1,45 @@
+import React, {Component} from "react";
+import './Dropdown.css';
+
+class Dropdown extends Component {
+    state = {
+        visible: false,
+    };
+
+    toogle = () => {
+        this.setState(prevState => ({
+            visible: !prevState.visible,
+        }));
+    };
+
+    // show = () => {
+    //     this.setState({ visible: true });
+    // };
+
+    // hide = () => {
+    //     this.setState({ visible: false });
+    // };
+
+    render() {
+        return (
+            <div className="Dropdown">
+                <button 
+                    type="button" 
+                    className="Dropdown__toggle" 
+                    onClick={this.toogle}>
+                        {this.state.visible ? 'Скрыть' : 'Показать'}
+                </button>
+
+                {/* <button type="button" className="Dropdown__toggle" onClick={this.hide}>
+                    Скрыть
+                </button> */}
+
+                {this.state.visible && (
+                    <div className="Dropdown__menu"><p>Выпадающее меню</p></div>
+                )}                
+            </div>
+        );
+    }
+}
+
+export default Dropdown;
