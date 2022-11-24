b13d33d373bd76078d851407efa4363aeeda15a6
TatianaKh0301 <92024678+TatianaKh0301@users.noreply.github.com>
2022-11-24 07:34:02 +0200

    update TodoList

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,12 +1,18 @@
 import React from "react";
+import './TodoList.css';
 
 const TodoList = ( {todos} ) => (
-    <ul>
+    <ul className="Todo__List">
         {todos.map(({ id, text}) => (
-            <li key={id}>
-                <p>{text}</p>
+            <li key={id} className="Todo__Item" style={{backgroundColor: `${getRandomHexColor()}`}}>
+                <p className="Todo__Text">{text}</p>
+                <button className="Todo__Button"> Delete</button>
             </li>
         ))}
     </ul>
 )
 export default TodoList;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
